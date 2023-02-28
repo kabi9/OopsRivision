c54cb8f3f90a87f1c8a2a838f11ba60de691fd6e
Kabindra Adhikari <kabi9>
2023-02-28 11:11:44 -0600

    OopsRivision

---
+++ src/oopsRevision/Interface.java
@@ -0,0 +1,29 @@
+package oopsRevision;
+
+public interface Interface {
+	
+	void Area(int length, int breadth);
+	}
+
+
+
+class Rectangle implements Interface {
+
+	@Override
+	public void Area(int length, int breadth) {
+		// TODO Auto-generated method stub
+	System.out.println("AREA OF RECTANGLE " + (length * breadth));
+	}
+	
+}
+
+class Square implements Interface {
+
+	@Override
+	public void Area(int length, int breadth) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Area of SQUARE " + (2* length * breadth) );
+	}
+	
+}
---
+++ src/oopsRevision/InterfaceA.java
@@ -0,0 +1,15 @@
+package oopsRevision;
+
+public class InterfaceA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle r = new Rectangle();
+		r.Area(4,2);
+		Square s = new Square();
+		s.Area(2, 1);
+
+	}
+
+}
